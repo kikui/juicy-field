5ce874c20b3b2dab5a6c7b45973d1c210dd4dafe
athery <a.thery@mavillemonshopping.fr>
2022-01-11 00:42:17 +0100

    little config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+@Injectable()
 export class AppComponent {
   title = 'juicy-field';
+  constructor(updates: SwUpdate) {
+    updates.versionUpdates.subscribe(event => {
+      if (event.type == "VERSION_DETECTED") {
+        updates.activateUpdate().then(() => document.location.reload());
+      }
+    });
+  }
 }
